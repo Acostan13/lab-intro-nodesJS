da665b5c0d9b820bc8e3d52624bb050a4215db7d
Acostan13 <acostan@fau.edu>
2020-03-02 17:55:46 -0500

    finished

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,17 +1,42 @@
 class SortedList {
-  constructor() {}
-
-  add(item) {}
-
-  get(pos) {}
-
-  max() {}
-
-  min() {}
-
-  sum() {}
-
-  avg() {}
+  constructor() {
+    this.items = []; // item array
+    this.length = this.items.length; // length of items
+  }
+  add(item) { //pushes and sorts items within array
+    this.items.push(item)
+    this.items.sort(function (a, b) {
+      return a - b;
+    })
+    this.length = this.items.length;
+  }
+  get(pos) { // get the value at index 'pos' in the list.
+    if (this.items[pos] === undefined) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos]
+  }
+  max() { // return the highest value of the array
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.max.apply(null, this.items)
+  }
+  min() { // return the lowest value of the array
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.min.apply(null, this.items)
+  }
+  sum() { // sums value of the array
+    if (this.items.length === 0) {
+      return 0;
+    }
+    return this.items.reduce((accumulator, currentValue) => accumulator + currentValue)
+  }
+  avg() { // average value of the array
+    return this.items.reduce((accumulator, currentValue) => accumulator + currentValue) / this.items.length
+  }
 }
 
 module.exports = SortedList;
